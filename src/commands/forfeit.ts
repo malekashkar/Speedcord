e9e42202b9868d6fd32555f95d6ab37565fe5d92
Malek Alashkar <malekalashkar@Maleks-MacBook-Pro.local>
2021-06-03 13:06:57 -0400

    :sparkles: Part of the forfeit command

---
+++ src/commands/forfeit.ts
@@ -0,0 +1,36 @@
+import { Message } from "discord.js";
+import Command from ".";
+import { RacerModel } from "../models/racer";
+import embeds from "../utils/embeds";
+
+export default class ForfeitCommand extends Command {
+  cmdName = "forfeit";
+  description = "Not doing so well? Drop the race whenever you need.";
+
+  async run(message: Message) {
+    const races = await RacerModel.find({
+      $or: [
+        { "userOne.racerId": message.author.id },
+        { "userTwo.racerId": message.author.id },
+      ],
+    });
+    if (!races.length)
+      return message.channel.send(
+        embeds.error(
+          `You're not in a race currently! You may ony forfeit if you are in one.`
+        )
+      );
+
+    for (const race of races) {
+      await race.deleteOne();
+      // Give the reward to the winner
+    }
+
+    return message.channel.send(
+      embeds.normal(
+        `Race Forfeited`,
+        `You have successfully forfeited your race! Sad to see you take a loss...`
+      )
+    );
+  }
+}
